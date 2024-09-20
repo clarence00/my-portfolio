a5a5742fcb4f7d800a273dd37b98435ff4dd91ce
clarence00 <clarencenativdad12@gmail.com>
2024-09-21 01:48:24 +0800

    [UPDATE]: Dot background updated

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -4,7 +4,8 @@ import HeroSection from "./components/HeroSection";
 
 export default function page() {
   return (
-    <div className="min-h-screen bg-customBlue-100">
+    <div className="min-h-screen dark:bg-customBlue-100  dark:bg-dot-customYellow-100/[0.2] relative">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <HeroSection />
       <Navbar />
     </div>
