48cf8fd9563ed2bbabec44a7819563188b4e8343
clarence00 <clarencenativdad12@gmail.com>
2024-09-22 03:38:21 +0800

    [ADD]: Experience section added

---
+++ app/(home)/components/ExperienceSection.js
@@ -0,0 +1,67 @@
+import React from "react";
+import ExperienceItem from "../ui/ExperienceItem";
+import Reveal from "../ui/Reveal";
+
+export default function ExperienceSection() {
+  const data = [
+    {
+      title: "Full-Stack Developer Intern",
+      company: "Achieve Without Borders, Inc,",
+      year: "August 2023 - September 2023",
+      tech: [
+        "ReactJS",
+        "Python",
+        "NodeJS",
+        "Bootstrap",
+        "Firebase",
+        "PostgreSQL",
+        "Odoo",
+        "VSCode",
+        "Git",
+      ],
+      details: [
+        "Collaborated with a team to fixed bugs and conducted quality assurance for the web application using ReactJS and Firebase before transitioning to the Odoo custom module project.",
+        "Developed custom module application for Odoo V16, including creating admin manual documentation and conducting benchmark tests.",
+        "Followed Scrum methodology, participating in daily stand-up meetings.",
+        "Assigned and documented tickets for project-related tasks and issues.",
+        "Utilized tech-stacks including ReactJS, Bootstrap, Firebase for web development and Python, NodeJS, PostgreSQL for Odoo module development. Worked with VSCode and Git for version control.",
+      ],
+    },
+    {
+      title: "Graphic Designer and Digital Marketing Intern",
+      company: "I-Map Websolutions, Inc,",
+      year: "August 2022 - September 2022",
+      tech: ["Canva", "Photoshop"],
+      details: [
+        "Collaborated with a team to design ad posters and promotional materials for an e-commerce platform",
+        "Managed company files to ensure smooth business operations.",
+        "Worked with Adobe Photoshop, Microsoft Word and PowerPoint",
+      ],
+    },
+  ];
+  return (
+    <div
+      className="flex-col flex justify-center mt-2 w-[70%] m-auto mb-20 rounded-2xl lg:flex-row"
+      id="experience">
+      <div className="m-6 text-center h-content md:text-left">
+        <Reveal>
+          <h1 className="pb-8 text-3xl font-bold md:text-3xl md:pl-2 text-customBlue-400 md:max-lg:px-6">
+            Experience
+          </h1>
+        </Reveal>
+        <div>
+          {data.map((item, idx) => (
+            <ExperienceItem
+              key={idx}
+              company={item.company}
+              title={item.title}
+              year={item.year}
+              tech={item.tech}
+              details={item.details}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/(home)/page.js
+++ app/(home)/page.js
@@ -2,15 +2,16 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
+import ExperienceSection from "./components/ExperienceSection";
 
 export default function page() {
   return (
     <div className="dark:bg-customBlue-100 relative dark:bg-dot-customYellow-100/[0.2]">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_60%,black)]"></div>
       <HeroSection />
+      <AboutSection />
+      <ExperienceSection />
       <Navbar />
-      <AboutSection />
-      <AboutSection />
     </div>
   );
 }
