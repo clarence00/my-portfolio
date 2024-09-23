5bc5e33d2cabf7ec096b07e8d8f50c49b61c04f9
clarence00 <clarencenativdad12@gmail.com>
2024-09-24 01:54:05 +0800

    [UPDATE]: Configuration and media images updated

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true, // Disables image optimization for static exports
+  },
   output: "export",
   reactStrictMode: true,
   webpack(config) {
